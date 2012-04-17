c3e246674991bfc0e2d18006b0f169696b7db69f
James Pellerano <james@japellerano.com>
2012-04-16 20:34:03 -0400

    initial commit

---
+++ rgbconvert.rb
@@ -0,0 +1,12 @@
+#! /usr/bin/ruby
+
+puts "Enter Red: "
+red = gets.to_i.to_s(16)
+
+puts "Enter Green: "
+green = gets.to_i.to_s(16)
+
+puts "Enter Blue: "
+blue = gets.to_i.to_s(16)
+
+puts red + green + blue
